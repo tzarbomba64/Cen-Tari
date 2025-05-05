c8021c513ce9b25eb9352a652f792b6b97f0e522
tzarbomba64 <zkwestmoreland@outlook.com>
2025-05-05 18:25:42 -0400

    Delete docs directory

--- docs/translator.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def translate(code:str) -> str:
-    # very naive mix of HTML/Java/C# style → Python
-    code = code.replace('var ', '')  # strip var
-    code = re.sub(r'//(.+)', r'#\1', code)  # comments
-    # TODO: add real parser for function(), class, etc.
-    return code
